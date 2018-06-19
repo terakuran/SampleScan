bc87a1ab82c9b0413a8501a9ff34eef3f1a3428f
terakuran <terakuran@nttdata.co.jp>
2018-06-19 20:08:47 +0900

    Add NullPointer Code

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,13 @@ public class Main {
     public static void main(String[] args) {
         // TODO:‚±‚ê‚©‚çŽn‚ß‚Ü‚·
         System.out.println("Hello");
+        
+        
+        // NullPointer
+        String str = null;
+        if (!str.isEmpty()) {
+            System.out.println(str);
+        }
 
     }
 
