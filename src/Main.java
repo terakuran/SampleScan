4e0a7e8f16e9c4e1b82f25573d6991d2f8d06bf5
terakuran <terakuran@nttdata.co.jp>
2018-06-27 14:35:08 +0900

    Fixing NullPointer and Embedding IndexOutOfBounds

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,26 @@
+import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
         // TODO:‚±‚ê‚©‚çn‚ß‚Ü‚·
         System.out.println("Hello");
-        
-        
-        // NullPointer
-        String str = null;
+
+        // NullPointer(Ver.2‚Å‰ğÁ)
+        String str = "Hello";
         if (!str.isEmpty()) {
             System.out.println(str);
+        }
+
+        // IndexOutObBounds(Ver.2‚Å–„‚ß‚İ)
+        ArrayList array = new ArrayList();
+
+        array.add("1”Ô–Ú");
+        array.add("2”Ô–Ú");
+        array.add("3”Ô–Ú");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(array.get(i));
         }
 
     }
